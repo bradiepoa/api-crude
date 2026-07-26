f0331736996c0ec9fd1d83282bb6eec3d6d48ca8
Bradiepoa <bradiepoa7@gmail.com>
2026-07-26 09:40:34 +0300

    react dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
 import Create from './components/Create'
+import Edit from './components/Edit'
+import Delete from './components/Delete'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/delete/:id" element={<Delete />} />
       </Routes>
     </>
   )
